9dba998a0a1b1bae342fccc208aea0b44948a8c8
Stanislav <slavi044@abv.bg>
2020-04-11 00:16:22 +0300

    Add homework

---
+++ C#OOP/TasksDuringTheHolidays/EncapsulationExercise/Problem01ClassBoxData/Problem01ClassBoxData/Box.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem01ClassBoxData
+{
+    class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        internal Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        internal double Length 
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        internal double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        internal double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public double surfaceArea()
+        {
+            double surfaceArea = 2 * this.length * this.width +
+                2 * this.length * this.height +
+                2 * this.width * this.height;
+
+            return surfaceArea;
+        }
+        public double LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * this.length * this.height +
+                2 * this.width * this.height;
+
+            return lateralSurfaceArea;
+        }
+        public double Volume()
+        {
+            double volume = this.length * this.width * this.height;
+
+            return volume;
+        }
+    }
+}
---
+++ C#OOP/TasksDuringTheHolidays/EncapsulationExercise/Problem01ClassBoxData/Problem01ClassBoxData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Problem01ClassBoxData
+{
+    class Program
+    {
+        static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.surfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.Volume():F2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
