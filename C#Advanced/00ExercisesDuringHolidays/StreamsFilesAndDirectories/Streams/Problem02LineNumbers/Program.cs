0dd052fafca66b5dd3ed6f8a995d53c740fe8d71
slavi044 <slavi044@abv.bg>
2020-08-16 15:11:02 +0300

    Add exercise

---
+++ C#Advanced/00ExercisesDuringHolidays/StreamsFilesAndDirectories/Streams/Problem02LineNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Problem02LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader streamReader = new StreamReader($"../../../text.txt");
+            StreamWriter streamWriter = new StreamWriter($"../../../newText.txt");
+
+            List<string> text = new List<string>();
+
+            using (streamReader)
+            {
+                int lineNumber = 0;
+
+                while (!streamReader.EndOfStream)
+                {
+                    lineNumber++;
+
+                    string line = $"Line{lineNumber}: {streamReader.ReadLine()}";
+
+                    text.Add(line);
+
+                    Console.WriteLine(line);
+                }
+            }
+
+            using (streamWriter)
+            {
+                foreach (var line in text)
+                {
+                    streamWriter.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine($"{Environment.NewLine}Ready!!!");
+        }
+    }
+}
