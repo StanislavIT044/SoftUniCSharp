a792fac2b3112be917b6c992371254fbfcf5bb84
slavi044 <slavi044@abv.bg>
2020-08-28 16:14:41 +0300

    Add exercises

---
+++ C#OOP/00ExercisesDuringHollidays/ReflectionAndAttributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,38 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using System.Linq;
+    using CommandPattern.Core.Commands;
+    using Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputArgs = args.Split(" ",
+                StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = inputArgs[0];
+            string[] commandArgs = inputArgs.Skip(1).ToArray();
+
+            ICommand command = null;
+
+            if (commandName == "Hello")
+            {
+                command = new HelloCommand();
+            }
+            else if (commandName == "Exit")
+            {
+                command = new ExitCommand();
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            string result = command.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
